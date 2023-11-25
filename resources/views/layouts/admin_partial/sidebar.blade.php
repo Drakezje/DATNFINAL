8766b8c822b74574a8acb761adb7d9ef68c2f25c
Drakezje <shyndeptrai01@gmail.com>
2023-11-25 13:38:34 +0700

    111
    1

--- resources/views/layouts/admin_partial/sidebar.blade.php
+++ resources/views/layouts/admin_partial/sidebar.blade.php
@@ -4,8 +4,8 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4 slimScrollBar" style="overflow-y: scroll;">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{($setting->favicon)}}"  class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">DATN</span>
+      <img src="{{url($setting->favicon)}}"  class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">LH Ecommerce</span>
     </a>
 
     <!-- Sidebar -->
@@ -51,13 +51,13 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{ route('subcategory.index') }} " class="nav-link">
+                <a href="{{ route('subcategory.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Danh mục phụ</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{ route('childcategory.index') }} " class="nav-link">
+                <a href="{{ route('childcategory.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Danh mục con</p>
                 </a>
@@ -69,15 +69,16 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{ route('warehouse.index') }} " class="nav-link">
+                <a href="{{ route('warehouse.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Kho</p>
                 </a>
               </li>
             </ul>
           </li>
-           @endif
-        @if(Auth::user()->product==1)
+          @endif
+
+          @if(Auth::user()->product==1)
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
@@ -90,7 +91,7 @@
               <li class="nav-item">
                 <a href="{{ route('product.create') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Sản phẩm mới</p>
+                  <p>Thêm sản phẩm</p>
                 </a>
               </li>
               <li class="nav-item">
@@ -103,12 +104,12 @@
           </li>
           @endif
 
-       @if(Auth::user()->offer==1)
+          @if(Auth::user()->offer==1)
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
-               Coupon/Chiến dịch
+               Quà tặng
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
@@ -122,13 +123,14 @@
               <li class="nav-item">
                 <a href="{{ route('campaign.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>E Chiến dịch</p>
+                  <p>Khuyến mãi</p>
                 </a>
               </li>
             </ul>
           </li>
           @endif
-      @if(Auth::user()->order==1)
+
+          @if(Auth::user()->order==1)
            <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
@@ -140,7 +142,7 @@
             <ul class="nav nav-treeview">
 
               <li class="nav-item">
-                <a href="{{ route('admin.order.index')  }}" class="nav-link">
+                <a href="{{ route('admin.order.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Các đơn hàng</p>
                 </a>
@@ -150,7 +152,7 @@
           @endif
 
 
-           @if(Auth::user()->blog==1)
+          @if(Auth::user()->blog==1)
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
@@ -161,22 +163,22 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href= "{{ route('admin.blog.category') }}" class="nav-link">
+                <a href="{{ route('admin.blog.category') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Danh mục</p>
+                  <p>Danh mục blog</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href=" {{ route('admin.blog.category')  }}" class="nav-link">
+                <a href="{{ route('admin.blog.category') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Blog</p>
                 </a>
               </li>
             </ul>
           </li>
-           @endif
+          @endif
 
-           @if(Auth::user()->pickup==1)
+          @if(Auth::user()->pickup==1)
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
@@ -194,9 +196,9 @@
               </li>
             </ul>
           </li>
-           @endif
+          @endif
 
-        @if(Auth::user()->ticket==1)
+          @if(Auth::user()->ticket==1)
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
@@ -207,16 +209,16 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href=" {{ route('ticket.index') }}" class="nav-link">
+                <a href="{{ route('ticket.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Phiếu</p>
                 </a>
               </li>
             </ul>
           </li>
-         @endif
+          @endif
 
-        @if(Auth::user()->contact==1)
+          @if(Auth::user()->contact==1)
            <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
@@ -234,53 +236,53 @@
               </li>
             </ul>
           </li>
-           @endif
+          @endif
 
-        @if(Auth::user()->report==1)
+          @if(Auth::user()->report==1)
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
-               Báo cáo
+               Phản hồi
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href=" {{ route('report.order.index')  }}" class="nav-link">
+                <a href="{{ route('report.order.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Báo cáo đơn hàng</p>
+                  <p>Đơn hàng phản hồi</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Người dùng báo cáo</p>
+                  <p>KH phản hồi</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Còn hàng báo cáo</p>
+                  <p>Còn hàng phản hồi</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Sản phẩm báo cáo</p>
+                  <p>Sản phẩm phản hồi</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Phiếu báo cáo</p>
+                  <p>Phiếu phàn hồi</p>
                 </a>
               </li>
             </ul>
           </li>
           @endif
 
-           @if(Auth::user()->setting==1)
+          @if(Auth::user()->setting==1)
            <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
@@ -299,7 +301,7 @@
               <li class="nav-item">
                 <a href="{{ route('website.setting') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Trang </p>
+                  <p>Cài đặt trang</p>
                 </a>
               </li>
               <li class="nav-item">
@@ -309,13 +311,13 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{ route('smtp.setting') }} " class="nav-link">
+                <a href="{{ route('smtp.setting') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>SMTP cài đặt</p>
+                  <p>Cài đặt SMTP</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href=" {{ route('payment.gateway') }} " class="nav-link">
+                <a href="{{ route('payment.gateway') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Cổng thanh toán</p>
                 </a>
@@ -329,7 +331,7 @@
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
-              Vai trò người dùng
+               Quản trị viên
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
@@ -337,18 +339,18 @@
               <li class="nav-item">
                 <a href="{{ route('create.role') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Tạo vai trò</p>
+                  <p>Tạo quản trị viên</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="{{ route('manage.role') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Quản lý vai trò</p>
+                  <p>Quản lý quản trị viên</p>
                 </a>
               </li>
             </ul>
           </li>
-         @endif
+          @endif
 
 
 
@@ -367,7 +369,6 @@
               <p class="text">Đăng xuất</p>
             </a>
           </li>
-
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
